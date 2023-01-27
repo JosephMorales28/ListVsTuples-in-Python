4474c2bd46ebb56fe5ebdbae7def03c31f32a6b1
Joseph Morales <117571756+JosephMorales28@users.noreply.github.com>
2023-01-27 16:56:39 +0800

    Update using for loop

--- ListVsTuples.py
+++ ListVsTuples.py
@@ -7,3 +7,21 @@ print(tuples[0],tuples[1],tuples[2],tuples[3],tuples[4])
 #Output
 #[1,2,3,4,5]
 #1 2 3 4 5
+print("List code")
+dimensions =[1,2,3]
+for dimension in dimensions:
+    print(dimension)
+print("Tuples code")
+dimensions1=(1,2,3)
+for d in dimensions1:
+    print(d)
+    
+#Output 
+#List code
+#1
+#2
+#3
+#Tuples code
+#1
+#2
+#3
